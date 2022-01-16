eaf058af179209e547f807e0bfe3c1ef47cfad95
Ömer Faruk İngeç <omer.ingec24@gmail.com>
2022-01-16 23:24:36 +0300

    First

---
+++ RabbitMQ.Watermark.Web/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Watermark.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Watermark.Web.BackgroundServices
+{
+    public class ImageWatermarkProcessBackgroundService : BackgroundService
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+        private readonly ILogger<ImageWatermarkProcessBackgroundService> _logger;
+        private IModel _channel;
+
+        public ImageWatermarkProcessBackgroundService(RabbitMQClientService rabbitMQClientService, ILogger<ImageWatermarkProcessBackgroundService> logger)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _channel = _rabbitMQClientService.Connect();
+            _channel.BasicQos(0, 1, false);
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+
+            _channel.BasicConsume(RabbitMQClientService.QueueName, false, consumer);
+
+            consumer.Received += Consumer_Received;
+
+
+            return Task.CompletedTask;
+        }
+
+        private Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            Task.Delay(10000).Wait();
+
+            try
+            {
+                var productImageCreatedEvent = JsonSerializer.Deserialize<productImageCreatedEvent>(Encoding.UTF8.GetString(@event.Body.ToArray()));
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", productImageCreatedEvent.ImageName);
+
+
+                var siteName = "www.omer_ingec.com";
+
+                //get orginal photo
+                using var img = Image.FromFile(path);
+
+                //created graphic to write something on image
+                using var graphic = Graphics.FromImage(img);
+
+                var font = new Font(FontFamily.GenericMonospace, 40, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                var textSize = graphic.MeasureString(siteName, font);
+
+                var color = Color.FromArgb(128, 255, 255, 255);
+                var brush = new SolidBrush(color);
+
+                var position = new Point(img.Width - ((int)textSize.Width + 30), img.Height - ((int)textSize.Height + 30));
+
+                graphic.DrawString(siteName, font, brush, position);
+
+                img.Save("wwwroot/Images/watermarks/" + productImageCreatedEvent.ImageName);
+
+                img.Dispose();
+                graphic.Dispose();
+
+                _channel.BasicAck(@event.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+
+            return Task.CompletedTask;
+            
+        }
+    }
+}
---
+++ RabbitMQ.Watermark.Web/Services/productImageCreatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Watermark.Web.Services
+{
+    public class productImageCreatedEvent
+    {
+        public string ImageName { get; set; }
+    }
+}
